c0f782cbd9ce7a3ad7295a12346714f67d985440
nickfro512 <nickfro@gmail.com>
2022-03-18 17:51:45 -0400

    Added source folder

---
+++ src/TitleView.java
@@ -0,0 +1,17 @@
+package com.dlft.dotlinefuntime;
+
+import android.content.Context;
+import android.view.View;
+
+public class TitleView extends View {
+
+
+
+    public TitleView(Context context) {
+        super(context);
+
+    }
+
+
+   
+}
